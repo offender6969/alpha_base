f2b20d48fd2f6b34d442af009271c29a5ae23d4f
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-21 06:02:44 +0800

    core: Extend pixel experience features
    * features were taken from xmlpak module, credits and thanks to: RadekBledowski
    
    test: add pixel_experience_2022_midyear.xml to product/etc/sysconfig, check if spoofing is broken, disable/enable spoofing, unlimited photo toggle working as intended on both scenarios.
    
    Co-authored-by: RadekBledowski <radekxmen.bledowski@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -54,6 +54,10 @@ public class PixelPropsUtils {
             "com.google.android.apps.privacy.wildlife",
             "com.google.android.apps.wallpaper",
             "com.google.android.apps.wallpaper.pixel",
+            "com.google.android.apps.subscriptions.red",
+            "com.google.android.dialer",
+            "com.google.android.messaging",
+            "com.google.android.googlequicksearchbox",
             "com.google.android.inputmethod.latin"
     };
 
@@ -85,7 +89,6 @@ public class PixelPropsUtils {
             "com.google.android.MTCL83",
             "com.google.android.UltraCVM",
             "com.google.android.apps.cameralite",
-            "com.google.android.dialer",
             "com.google.android.euicc",
             "com.google.ar.core",
             "com.google.android.youtube",
@@ -95,7 +98,6 @@ public class PixelPropsUtils {
             "com.google.android.apps.recorder",
             "com.google.android.apps.restore",
             "com.google.android.apps.wearables.maestro.companion",
-            "com.google.android.apps.subscriptions.red",
             "com.google.android.apps.tachyon",
             "com.google.android.apps.tycho",
             "com.google.android.setupwizard"
